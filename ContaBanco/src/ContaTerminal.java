8bf574821394d7ad161b5cc2f6f3f25443fc7263
Ana Carolina de Sousa Camilo <anacarolinacamilo31@gmail.com>
2024-06-07 21:18:57 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -14,7 +14,7 @@ public class ContaTerminal { public static void main(String[] args) {
         System.out.println("Por favor, digite o número de usuário! ");
         usuario = scan.nextInt();
         System.out.println("Por favor, digite o seu saldo: ");
-        saldo = scan.nextDouble(); // o scanner varíavel double não está funcionando na minha máquina mas creio que o código esteja correto
+        saldo = scan.nextDouble(); 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia +", conta " + usuario + " e seu saldo " + saldo + " já está disponível para saque!");
 
     }
