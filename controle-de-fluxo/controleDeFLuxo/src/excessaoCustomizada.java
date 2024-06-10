084dc72e2975419934d19ca00af2a83548b32fc0
Ana Carolina de Sousa Camilo <anacarolinacamilo31@gmail.com>
2024-06-09 21:43:33 -0300

    Estudo sobre Tratamento de exceções em JAVA

---
+++ controle-de-fluxo/controleDeFLuxo/src/excessaoCustomizada.java
@@ -0,0 +1,13 @@
+public class excessaoCustomizada extends Exception {public static void main(String[] args) {
+   String cepFormatado = formatarCep (cep);
+}  
+
+static String formatarCep(String cep) throws CepInvalidoException {
+if(cep.length() != 8)
+throw new CepInvalidoException();
+
+//simulando um cep formatado
+return "23.765-064";    
+}
+
+}
